c0adf1265ab71305e54eb032cf2abc4c78135118
Marcelo <amigomef@gmail.com>
2017-02-25 20:47:28 -0300

    First commit

---
+++ Source/HowTo_AutoCamera/HowTo_AutoCameraGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "HowTo_AutoCameraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOWTO_AUTOCAMERA_API AHowTo_AutoCameraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
